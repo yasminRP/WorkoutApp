5b6ca7046a93f86c760d8a609df2896ea1b40b19
yasminRP <22026024@myrp.edu.sg>
2025-01-16 11:13:16 +0800

    Update workouts.js

--- mern-workout-app-starter-main/mern-workout-app-starter-main/backend/routes/workouts.js
+++ mern-workout-app-starter-main/mern-workout-app-starter-main/backend/routes/workouts.js
@@ -10,13 +10,27 @@ const {
 const router = express.Router();
 
 // Get all workouts
-router.get("/", getWorkouts);
+router.get('/', (req,res) => {
+  res.json({mssg:'Get all workouts'})
+})
+
 
 // Get a single workout
-router.get("/:id", getWorkout);
+router.get('/:id', (req,res) => {
+  res.json({mssg:'Get a single workout'})
+})
 
 // Post a new workout
-router.post("/", createWorkout);
+router.get('/', async (req,res) => {
+  const {title,load,reps} = req.body
+
+  try {
+    const workout = await Workout.create({title, load, reps})
+    res.status(200).json(workout)
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+})
 
 // Delete a workout
 router.delete("/:id", deleteWorkout);
