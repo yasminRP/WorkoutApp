1be35117b51a20e99f5fcbfb6d8bd5f18c29c399
yasminRP <22026024@myrp.edu.sg>
2025-01-16 11:33:28 +0800

    Update workouts.js

--- mern-workout-app-starter-main/mern-workout-app-starter-main/backend/routes/workouts.js
+++ mern-workout-app-starter-main/mern-workout-app-starter-main/backend/routes/workouts.js
@@ -10,36 +10,18 @@ const {
 const router = express.Router();
 
 // Get all workouts
-router.get('/', (req,res) => {
-  res.json({mssg:'Get all workouts'})
-})
-
+router.get("/", getWorkouts);
 
 // Get a single workout
-router.get('/:id', (req,res) => {
-  res.json({mssg:'Get a single workout'})
-})
+router.get("/:id", getWorkout);
 
 // Post a new workout
-router.post('/', async (req,res) => {
-  const {title,load,reps} = req.body
-
-  try {
-    const workout = await Workout.create({title, load, reps})
-    res.status(200).json(workout)
-  } catch (error) {
-    res.status(400).json({error: error.message})
-  }
-})
+router.post("/", createWorkout);
 
 // Delete a workout
-router.delete('/:id', (req,res) => {
-  res.json({mssg:'Delete a workout'})
-})
+router.delete("/:id", deleteWorkout);
 
 // Update a workout
-router.patch("/:id", (req,res) => {
-  res.json({mssg:'Update a workout'})
-});
+router.patch("/:id", updateWorkout);
 
 module.exports = router;
